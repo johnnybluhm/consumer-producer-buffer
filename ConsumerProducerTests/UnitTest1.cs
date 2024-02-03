0e7e203784546959b536faa54a2019a7d6255a0d
johnnybluhm <54964159+johnnybluhm@users.noreply.github.com>
2024-02-03 14:32:00 -0700

    Add code to time

--- ConsumerProducerTests/UnitTest1.cs
+++ ConsumerProducerTests/UnitTest1.cs
@@ -1,15 +1,32 @@
 using ConsumerProducer;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Xunit;
 
 namespace ConsumerProducerTests
 {
     public class UnitTest1
     {
+        private Stopwatch stopWatch = new Stopwatch();
+        private List<long> timesInMs = new List<long>();
+        private const int LOOP_COUNT = 10; 
+
         [Fact]
-        public void Test1()
+        public void CalculateBaseLineAvgTime()
         {
-            var test = new Baseline();
-            test.ResolveNames();
+            long totalTimeMs = 0;
+            for(int i = 0; i < LOOP_COUNT; i++)
+            {
+                var test = new Baseline();
+                stopWatch.Start();
+                test.LoadFiles();
+                test.ResolveNames();
+                stopWatch.Stop();
+                totalTimeMs += stopWatch.ElapsedMilliseconds;
+                timesInMs.Add(stopWatch.ElapsedMilliseconds);
+                stopWatch.Reset();
+            }
+            var averageTime = totalTimeMs / LOOP_COUNT;
         }
     }
 }
